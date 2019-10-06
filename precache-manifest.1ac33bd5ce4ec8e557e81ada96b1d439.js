1119fccc7adf776dc4f4982f431bab4a53e65f1b
Hernán R <hernan@null.net>
2019-10-06 00:04:12 -0400

    deploy

---
+++ precache-manifest.1ac33bd5ce4ec8e557e81ada96b1d439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655b84b52e28ddee5ee9088b0e4bb5bf",
+    "url": "/ig-caption-fixer/index.html"
+  },
+  {
+    "revision": "e00d8df002f329f7ed4a",
+    "url": "/ig-caption-fixer/static/css/main.a3dc3831.chunk.css"
+  },
+  {
+    "revision": "78a669068f878598de4f",
+    "url": "/ig-caption-fixer/static/js/2.665014fb.chunk.js"
+  },
+  {
+    "revision": "e00d8df002f329f7ed4a",
+    "url": "/ig-caption-fixer/static/js/main.72af945d.chunk.js"
+  },
+  {
+    "revision": "5cccef263dd603f89ad1",
+    "url": "/ig-caption-fixer/static/js/runtime-main.68fab0b6.js"
+  }
+]);
