dfe52c09f220ae8c943051cc997625f005db4ce7
Hernán R <hernan@null.net>
2019-10-06 00:24:33 -0400

    Fix homepage

---
+++ precache-manifest.3b7a1572be9d90d0a2999accca2141df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1a0ef5d8c72239700d4e4c1c31d24a",
+    "url": "/ig-caption-fixer/index.html"
+  },
+  {
+    "revision": "6b22167281594f853a57",
+    "url": "/ig-caption-fixer/static/css/main.d414563c.chunk.css"
+  },
+  {
+    "revision": "78a669068f878598de4f",
+    "url": "/ig-caption-fixer/static/js/2.665014fb.chunk.js"
+  },
+  {
+    "revision": "6b22167281594f853a57",
+    "url": "/ig-caption-fixer/static/js/main.72af945d.chunk.js"
+  },
+  {
+    "revision": "5cccef263dd603f89ad1",
+    "url": "/ig-caption-fixer/static/js/runtime-main.68fab0b6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77d967560a8ebaff8cb53b13bc6e9d37.js"
+  "/ig-caption-fixer/precache-manifest.3b7a1572be9d90d0a2999accca2141df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ig-caption-fixer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
